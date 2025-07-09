44b2a9cad12aa47edc1cecd7528e227f39f1623b
Frankie Steinlie <frankie.intern24slides@gmail.com>
2025-07-09 09:17:01 +0700

    database connection

--- Config/database.js
+++ Config/database.js
@@ -4,7 +4,7 @@ const mysql = require('mysql');
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: 'Shacia1858',
+  password: '',
   database: 'project_sewasepeda'
 });
 
